149ed50d22edbc1bcdeca9d54525f3affa29af04
SGrinstead <seth.grinstead3299@gmail.com>
2023-10-16 13:16:07 -0700

    Separated OrdersController Create action into multiple methods

--- DisorderedOrdersMVC/Controllers/OrdersController.cs
+++ DisorderedOrdersMVC/Controllers/OrdersController.cs
@@ -3,6 +3,7 @@ using DisorderedOrdersMVC.Models;
 using DisorderedOrdersMVC.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
 
 namespace DisorderedOrdersMVC.Controllers
 {
@@ -28,53 +29,16 @@ namespace DisorderedOrdersMVC.Controllers
         public IActionResult Create(IFormCollection collection, string paymentType)
         {
             // create order
-            int customerId = Convert.ToInt16(collection["CustomerId"]);
-            Customer customer = _context.Customers.Find(customerId);
-            var order = new Order() { Customer = customer };
-            for (var i = 1; i < collection.Count - 1; i++)
-            {
-                var kvp = collection.ToList()[i];
-                if (kvp.Value != "0")
-                {
-                    var product = _context.Products.Where(p => p.Name == kvp.Key).First();
-                    var orderItem = new OrderItem() { Item = product, Quantity = Convert.ToInt32(kvp.Value) };
-                    order.Items.Add(orderItem);
-                }
-            }
+            var order = CreateOrderFromCollection(collection);
 
             // verify stock available
-            foreach (var orderItem in order.Items)
-            {
-                if (!orderItem.Item.InStock(orderItem.Quantity))
-                {
-                    orderItem.Quantity = orderItem.Item.StockQuantity;
-                }
-
-                orderItem.Item.DecreaseStock(orderItem.Quantity);
-            }
+            VerifyStockAvailable(order);
 
             // calculate total price
-            var total = 0;
-            foreach (var orderItem in order.Items)
-            {
-                var itemPrice = orderItem.Item.Price * orderItem.Quantity;
-                total += itemPrice;
-            }
+            var total = CalculateTotalPrice(order);
 
             // process payment
-            IPaymentProcessor processor;
-            if (paymentType == "bitcoin")
-            {
-                processor = new BitcoinProcessor();
-            }
-            else if (paymentType == "paypal")
-            {
-                processor = new PayPalProcessor();
-            }
-            else
-            {
-                processor = new CreditCardProcessor();
-            }
+            IPaymentProcessor processor = GetPaymentProcessor(paymentType);
 
             processor.ProcessPayment(total);
 
@@ -103,5 +67,64 @@ namespace DisorderedOrdersMVC.Controllers
 
             return View(order);
         }
+
+        private Order CreateOrderFromCollection(IFormCollection collection)
+        {
+			int customerId = Convert.ToInt16(collection["CustomerId"]);
+			Customer customer = _context.Customers.Find(customerId);
+
+			var order = new Order() { Customer = customer };
+			for (var i = 1; i < collection.Count - 1; i++)
+			{
+				var kvp = collection.ToList()[i];
+				if (kvp.Value != "0")
+				{
+					var product = _context.Products.Where(p => p.Name == kvp.Key).First();
+					var orderItem = new OrderItem() { Item = product, Quantity = Convert.ToInt32(kvp.Value) };
+					order.Items.Add(orderItem);
+				}
+			}
+            return order;
+		}
+
+        private IPaymentProcessor GetPaymentProcessor(string paymentType)
+        {
+			if (paymentType == "bitcoin")
+			{
+				return new BitcoinProcessor();
+			}
+			else if (paymentType == "paypal")
+			{
+				return new PayPalProcessor();
+			}
+			else
+			{
+				return new CreditCardProcessor();
+			}
+		}
+
+        private int CalculateTotalPrice(Order order)
+        {
+            int total = 0;
+			foreach (var orderItem in order.Items)
+			{
+				var itemPrice = orderItem.Item.Price * orderItem.Quantity;
+				total += itemPrice;
+			}
+            return total;
+		}
+
+        private void VerifyStockAvailable(Order order)
+        {
+			foreach (var orderItem in order.Items)
+			{
+				if (!orderItem.Item.InStock(orderItem.Quantity))
+				{
+					orderItem.Quantity = orderItem.Item.StockQuantity;
+				}
+
+				orderItem.Item.DecreaseStock(orderItem.Quantity);
+			}
+		}
     }
 }
